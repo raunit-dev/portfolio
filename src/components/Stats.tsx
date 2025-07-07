1fd10fa782f9d34614b434d44ad188339b3dfc04
raunit-dev <raunitjaiswal510@gmail.com>
2025-07-08 04:31:49 +0530

    fixed github stats bug

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import Image from 'next/image';
 
 
 
@@ -8,7 +7,7 @@ export default function Stats() {
     <Card className="md:col-span-3 md:row-span-2">
       <CardContent className="space-y-2">
         <div className="mt-4">
-          <Image 
+          <img 
             src="https://github-readme-stats.vercel.app/api?username=raunit-dev&theme=dark&hide_border=false&include_all_commits=false&count_private=false"
             alt="GitHub Stats"
             className="rounded-lg"
